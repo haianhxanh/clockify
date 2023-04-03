343657bd9eba3aeb5aeb6038e03dfc8fb7769d5f
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-04-03 18:17:11 +0200

    Pages

---
+++ frontend/src/pages/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SideMenu = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Tasks</li>
+        <li>Reports</li>
+        <li>Sharing</li>
+        <li>Team</li>
+        <li>Calendar</li>
+        <li>My Account</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideMenu;
